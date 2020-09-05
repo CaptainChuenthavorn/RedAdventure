102bbf30eb21eed62efd68b923493cf7f5460ec4
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-09-05 19:13:42 +0700

    complete

--- RedAdventure/Source.cpp
+++ RedAdventure/Source.cpp
@@ -1,16 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdlib.h>
 #include <stdio.h>
 #include<windows.h>
 #include<conio.h>
-//void clrscr();
 void erase_ship(int , int); 
 void draw_ship(int ,int);
-//void gotoxy(int, int);
 int main()
 {
 		char ch = ' ';
-		int x = 75, y = 2;
+		int x = 20, y = 5;
 		draw_ship(x, y);
 		do {
 			if (_kbhit()) {
@@ -18,40 +15,36 @@ int main()
 				if (ch == 'a') {
 					erase_ship(x, y);
 					draw_ship(--x, y);
-					
 				}
 				if (ch == 'd') { 
 					erase_ship(x, y);
 					draw_ship(++x, y);
-					
 				}
 				if (ch == 'w') {
 					erase_ship(x, y);
 					draw_ship(x, --y);
-					
 				}
 				if (ch == 's') {
 					erase_ship(x, y);
 					draw_ship(x, ++y);
-					
 				}
 				fflush(stdin);
-				if (x < 0) {
+				if (x < 0) {		 //if out of monitor x<0
 					erase_ship(x+6, y);
 					x = 0;
 					draw_ship(x, y);
 				}
-				if (x > 80) {
+				if (x > 80) {        //if out of monitor x>80
 					erase_ship(x , y);
 					x = 80;
 					draw_ship(x, y);
 				}
-				if (y < 0) {
+				if (y < 0) {		//if out of monitor y<0
 					erase_ship(x + 5, y+1);
 					y = 0;
 					draw_ship(x, y);
 				}
-				if (y > 80) {
+				if (y > 80) {		//if out of monitor y>80
 					erase_ship(x, y);
 					y = 80;
 					draw_ship(x, y);
@@ -71,8 +64,3 @@ void erase_ship(int x, int y) {
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 	printf("     ");
 }
-/*
-void clrscr()
-{
-	system("@cls||clear");
-}*/
