97a38b5e29152ce30abfb9970d91afcecc813c38
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-09-05 16:35:57 +0700

    create clean screen

--- RedAdventure/Source.cpp
+++ RedAdventure/Source.cpp
@@ -1,22 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
 #include <stdio.h>
 #include<windows.h>
 #include<conio.h>
+void clrscr();
+void erase_ship(int , int); 
 void draw_ship(int ,int);
 void gotoxy(int, int);
 int main()
 {
 		char ch = ' ';
-		int x = 38, y = 20;
+		int x = 5, y = 20;
 		draw_ship(x, y);
 		do {
-			if (_kbhit()) {
+			if (_kbhit() && x>=0 &&x<=80) {
 				ch = _getch();
-				if (ch == 'a') { draw_ship(--x, y); }
-				if (ch == 'd') { draw_ship(++x, y); }
+				if (ch == 'a') { //clrscr();
+				draw_ship(--x, y); }
+				if (ch == 'd') { //clrscr();
+				draw_ship(++x, y); }
+				if (ch == 'w') { 
+
+					//clrscr();
+					draw_ship(x, --y);
+				}
+				if (ch == 's') { //clrscr();
+				draw_ship(x, ++y); }
 				fflush(stdin);
+				if (x < 0) x = 0;
+				if (x > 80) x = 80;
 			}
-			Sleep(100);
+			Sleep(50);
 		} while (ch != 'x');
 		return 0;
 }
@@ -29,4 +43,14 @@ void gotoxy(int x, int y)
 {
 	COORD c = { x, y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+void erase_ship(int x, int y) {
+	printf("    ");
+	COORD c = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+void clrscr()
+{
+	system("@cls||clear");
 }
