100628d768961961e78eee4101c635807a3a8681
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-09-30 13:23:17 +0700

    add bullet sucess

--- RedAdventure/third.cpp
+++ RedAdventure/third.cpp
@@ -13,30 +13,81 @@ void draw_bullet(int, int);
 void clear_bullet(int, int);
 int main()
 {
-		char ch = ' ';
-		int x = 20, y = 20, direction = 0,bullet = 0,Bx=x+2,By=y-1;
-		setcolor(4, 2);
-		setcursor(0);
-		draw_ship(x, y);
-		do {
-			if (_kbhit()) {
-				ch = _getch();
-				if (ch == 'a') {
-					direction = 1;
-				}
-				if (ch == 'd') {
-					direction = 2;
-				}
-				if (ch == 's') {
-					direction = 0;
-				}
-				if (ch == ' ') {
-					bullet = 1;
-				}
-				setcolor(4, 2);
-				fflush(stdin);
-				
+	char ch = ' ';
+	int x = 20, y = 20, direction = 0, bullet = 0, Bx = x + 2, By = y - 1,ammo=5;
+	setcolor(4, 2);
+	setcursor(0);
+	draw_ship(x, y);
+	do {
+		if (_kbhit()) {
+			ch = _getch();
+			if (ch == 'a') {
+				direction = 1;
 			}
+			if (ch == 'd') {
+				direction = 2;
+			}
+			if (ch == 's') {
+				direction = 0;
+			}
+			if (ch == ' ') {
+				bullet = 1;
+			}
+			setcolor(4, 2);
+			fflush(stdin);
+
+		}
+		if (direction == 1) {
+			erasecolor(0, 0);
+			erase_ship(x, y);
+			setcolor(4, 2);
+			draw_ship(--x, y);
+		}
+		if (direction == 2) {
+			erasecolor(0, 0);
+			erase_ship(x, y);
+			setcolor(4, 2);
+			draw_ship(++x, y);
+		}
+		if (direction == 0) {
+
+			erase_ship(x, y);
+
+			draw_ship(x, y);
+		}
+		if (bullet == 1 && ammo >0 ) {
+			Bx = x + 2, By = y - 1;
+
+			while (By != 0) {
+				erasecolor(0, 0);
+				clear_bullet(Bx, By);
+				setcolor(4, 2);
+				draw_bullet(Bx, --By);
+
+				if (By == 0) {
+					erasecolor(0, 0);
+					clear_bullet(Bx, By);
+					setcolor(4, 2);
+					draw_bullet(Bx, By);
+					Sleep(50);
+					erasecolor(0, 0);
+					clear_bullet(Bx, By);
+					setcolor(4, 2);
+				}
+				if (x < 0) {		 //if out of monitor x<0
+					erasecolor(0, 0);
+					erase_ship(x + 6, y);
+					setcolor(4, 2);
+					x = 0;
+					draw_ship(x, y);
+				}
+				if (x > 80) {        //if out of monitor x>80
+					erasecolor(0, 0);
+					erase_ship(x, y);
+					x = 80;
+					setcolor(4, 2);
+					draw_ship(x, y);
+				}
 			if (direction == 1) {
 				erasecolor(0, 0);
 				erase_ship(x, y);
@@ -50,60 +101,44 @@ int main()
 				draw_ship(++x, y);
 			}
 			if (direction == 0) {
-				
+
 				erase_ship(x, y);
-				
+
 				draw_ship(x, y);
 			}
-			if (bullet == 1) {
-				Bx = x + 2, By = y - 1;
-				
-				while (By != 0 ) {
-					erasecolor(0, 0);
-					clear_bullet(Bx, By);
-					setcolor(4, 2);
-					draw_bullet(Bx, --By);
-					
-					if (By == 0) {
-						erasecolor(0, 0);
-						clear_bullet(Bx, By);
-						setcolor(4, 2);
-						draw_bullet(Bx, By);
-						Sleep(50);
-						erasecolor(0, 0);
-						clear_bullet(Bx, By);
-						setcolor(4, 2);
-					}
-					Sleep(50);
-				}
-				bullet--;
-				
+			Sleep(50);
+			
 			}
-			if (x < 0) {		 //if out of monitor x<0
-				erasecolor(0, 0);
-				erase_ship(x + 6, y);
-				setcolor(4, 2);
-				x = 0;
-				draw_ship(x, y);
-			}
-			if (x > 80) {        //if out of monitor x>80
-				erasecolor(0, 0);
-				erase_ship(x, y);
-				x = 80;
-				setcolor(4, 2);
-				draw_ship(x, y);
-			}
-			if (y < 0) {		//if out of monitor y<0
-				erase_ship(x + 5, y + 1);
-				y = 0;
-				draw_ship(x, y);
-			}
-			if (y > 80) {		//if out of monitor y>80
-				erase_ship(x, y);
-				y = 80;
-				draw_ship(x, y);
-			}
-			Sleep(100);
+			ammo--;
+			bullet = 0;
+		} 
+		{
+		if (x < 0) {		 //if out of monitor x<0
+			erasecolor(0, 0);
+			erase_ship(x + 6, y);
+			setcolor(4, 2);
+			x = 0;
+			draw_ship(x, y);
+		}
+		if (x > 80) {        //if out of monitor x>80
+			erasecolor(0, 0);
+			erase_ship(x, y);
+			x = 80;
+			setcolor(4, 2);
+			draw_ship(x, y);
+		}
+		if (y < 0) {		//if out of monitor y<0
+			erase_ship(x + 5, y + 1);
+			y = 0;
+			draw_ship(x, y);
+		}
+		if (y > 80) {		//if out of monitor y>80
+			erase_ship(x, y);
+			y = 80;
+			draw_ship(x, y);
+		}
+}
+			Sleep(50);
 		} while (ch != 'x');
 		return 0;
 }
